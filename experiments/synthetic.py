6602a9ad2ba1e842d33f9e2844122f0b80f63d69
hr0nix <boris.jangel@gmail.com>
2023-02-05 01:05:12 +0000

    Fix entropy again.

--- experiments/synthetic.py
+++ experiments/synthetic.py
@@ -274,7 +274,7 @@ def compute_accuracy_entropy(
                     token_log_probs[0, -1, token_index] = -100000
                 token_log_probs = F.log_softmax(token_log_probs, dim=-1)
 
-            entropy = th.distributions.Categorical(logits=token_log_probs).entropy()
+            entropy = th.distributions.Categorical(logits=token_log_probs[0, -1]).entropy()
             entropy_sum += float(entropy.cpu().item())
             token_probs = np.exp(token_log_probs[0, -1].cpu().numpy())
             sampled_token = np.random.choice(len(token_probs), p=token_probs)
